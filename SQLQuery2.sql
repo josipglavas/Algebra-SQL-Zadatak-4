4793a18df9b48bfd1b6e615ef88e8ccf3b36f9ab
josipglavas <glavas.josip@icloud.com>
2025-05-22 20:23:46 +0200

    rjesen zadatak

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -3,34 +3,34 @@ GO
 
 BEGIN TRAN
     INSERT INTO Osoba(Ime) VALUES ('Marko');
-    SAVE TRAN SavePoint1;
+    SAVE TRAN SavePoint1
 
     INSERT INTO Osoba (Ime) VALUES ('Jelena');
-	SAVE TRAN SavePoint2;
+	SAVE TRAN SavePoint2
 ROLLBACK TRAN
 
 BEGIN TRAN
     INSERT INTO Osoba (Ime) VALUES ('Ivana');
-    SAVE TRAN SavePoint1;
+    SAVE TRAN SavePoint1
 
     INSERT INTO Osoba (Ime) VALUES ('Nikola');
-    SAVE TRAN SavePoint2;
+    SAVE TRAN SavePoint2
 COMMIT TRAN
 
 BEGIN TRAN
     INSERT INTO Osoba (Ime) VALUES ('Petar');
-    SAVE TRAN SavePoint1;
+    SAVE TRAN SavePoint1
 
     INSERT INTO Osoba (Ime) VALUES ('Josip');
-    ROLLBACK TRAN SavePoint1;
+    ROLLBACK TRAN SavePoint1
 ROLLBACK TRAN
 
 BEGIN TRAN
     INSERT INTO Osoba (Ime) VALUES ('Maja');
-    SAVE TRAN SavePoint1;
+    SAVE TRAN SavePoint1
 
     INSERT INTO Osoba (Ime) VALUES ('Stjepan');
-    ROLLBACK TRAN SavePoint1;
+    ROLLBACK TRAN SavePoint1
 COMMIT TRAN
 
 
