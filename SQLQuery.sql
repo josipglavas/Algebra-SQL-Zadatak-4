a1db02142f940949dd28b136593c48090a8eac64
josipglavas <glavas.josip@icloud.com>
2025-05-22 19:29:49 +0200

    dodan include(komentar)

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -5,7 +5,7 @@ SET STATISTICS IO ON
 GO
 
 CREATE NONCLUSTERED INDEX Index_Racun_DatumIzdavanja
-ON racun (DatumIzdavanja)
+ON racun (DatumIzdavanja) INCLUDE (Komentar);
 
 SELECT IDRacun, DatumIzdavanja, Komentar
 FROM racun
@@ -13,5 +13,5 @@ WHERE DatumIzdavanja BETWEEN '20010702' AND '20010702 23:59:59'
 
 DROP INDEX Index_Racun_DatumIzdavanja ON racun
 -- 202 logical reads prije dodavanja indexa
--- 10 logical reads nakon dodavanja indexa
+-- 2 logical reads nakon dodavanja indexa
 
