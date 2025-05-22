d9db5a8e212fabd62480160306cb63527efa0e10
josipglavas <glavas.josip@icloud.com>
2025-05-22 20:15:36 +0200

    rjesen demo 2

---
+++ SQLQuery2.sql
@@ -0,0 +1,36 @@
+USE AdventureWorksOBP
+GO
+
+BEGIN TRAN
+    INSERT INTO Osoba(Ime) VALUES ('Marko');
+    SAVE TRAN SavePoint1;
+
+    INSERT INTO Osoba (Ime) VALUES ('Jelena');
+ROLLBACK TRAN
+
+BEGIN TRAN
+    INSERT INTO Osoba (Ime) VALUES ('Ivana');
+    SAVE TRAN SavePoint1;
+
+    INSERT INTO Osoba (Ime) VALUES ('Nikola');
+    SAVE TRAN SavePoint2;
+COMMIT TRAN
+
+BEGIN TRAN
+    INSERT INTO Osoba (Ime) VALUES ('Petar');
+    SAVE TRAN SavePoint1;
+
+    INSERT INTO Osoba (Ime) VALUES ('Josip');
+    ROLLBACK TRAN SavePoint1;
+ROLLBACK TRAN
+
+BEGIN TRAN
+    INSERT INTO Osoba (Ime) VALUES ('Maja');
+    SAVE TRAN SavePoint1;
+
+    INSERT INTO Osoba (Ime) VALUES ('Stjepan');
+    ROLLBACK TRAN SavePoint1;
+COMMIT TRAN
+
+
+
