07d6a1e86348de3fcbe63b28124943a50011a46c
josipglavas <glavas.josip@icloud.com>
2025-05-22 19:09:10 +0200

    dodan neklasterizirani index na datum izdavanja racuna

---
+++ SQLQuery.sql
@@ -0,0 +1,17 @@
+USE AdventureWorksOBP
+GO
+
+SET STATISTICS IO ON
+GO
+
+CREATE NONCLUSTERED INDEX Index_Racun_DatumIzdavanja
+ON racun (DatumIzdavanja)
+
+SELECT IDRacun, DatumIzdavanja, Komentar
+FROM racun
+WHERE DatumIzdavanja BETWEEN '20010702' AND '20010702 23:59:59'
+
+DROP INDEX Index_Racun_DatumIzdavanja ON racun
+-- 202 logical reads prije dodavanja indexa
+-- 10 logical reads nakon dodavanja indexa
+
